82c00c3bbbf9eaeefe6bda4b09a4623f768dd663
Misha Ovsepyan <m1shkaa666@gmail.com>
2020-07-28 21:03:10 +0300

    simple app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ContactCard from "./components/ContactCard";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const fetchUsers = async () => {
+    const response = await fetch("https://randomuser.me/api/?results=3");
+    const data = await response.json();
+
+    setContacts(data.results);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {contacts.map((contact, index) => (
+        <ContactCard
+          key={index}
+          avatar={contact.picture.large}
+          name={`${contact.name.first} ${contact.name.last}`}
+          email={contact.email}
+          age={contact.dob.age}
+        />
+      ))}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/ContactCard.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const ContactCard = (props) => {
+  const { avatar, name, email, age } = props;
+
+  const [showAge, setShowAge] = useState(true);
+
+  const handleButtonClick = () => {
+    setShowAge(!showAge);
+  };
+
+  return (
+    <div className="contact-card">
+      <img src={avatar} alt="profile" />
+      <div className="user-details">
+        <p>{name}</p>
+        <p>Email: {email}</p>
+        <button onClick={handleButtonClick}>Toggle Age</button>
+        {showAge ? <p>Age: {age}</p> : null}
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
